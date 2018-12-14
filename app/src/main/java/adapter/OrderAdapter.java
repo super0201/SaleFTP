051ccd47be6829771d259cc468ac38426d339e68
hiepacex <45091842+hiepacex@users.noreply.github.com>
2018-12-14 15:28:42 +0700

    Z

--- app/src/main/java/adapter/OrderAdapter.java
+++ app/src/main/java/adapter/OrderAdapter.java
@@ -1,31 +1,43 @@
 package adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.team2.saleftp.R;
 
 import java.util.ArrayList;
 
+import dao.ProductDAO;
 import model.Cart;
+import model.Product;
 
 public class OrderAdapter extends BaseAdapter {
 
-    Context context;
+    private Context context;
+    private LayoutInflater inflater;
     ArrayList<Cart> arrCart;
+    ProductDAO dao;
 
     public OrderAdapter(Context context, ArrayList<Cart> arrayCart) {
-
+        super();
+        this.context = context;
+        this.arrCart = arrayCart;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return arrCart.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return arrCart.get(position);
     }
 
     @Override
@@ -33,8 +45,29 @@ public class OrderAdapter extends BaseAdapter {
         return 0;
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public static class ViewHolder {
+        TextView tvNameP, tvAmountP, tvPriceP;
+        ImageView imvIconP;
     }
+
+    @Override
+    /*public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.item_order, null);
+            holder.imvIconP = (ImageView) convertView.findViewById(R.id.imvPOrder);
+            holder.tvNameP = (TextView) convertView.findViewById(R.id.tvNamePOrder);
+            holder.tvAmountP = (TextView) convertView.findViewById(R.id.tvAmountPOrder);
+            holder.tvPriceP = (TextView) convertView.findViewById(R.id.tvPricePOrder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        Cart entry = (Cart) arrCart.get(position);
+        holder.imvIconP.setImageResource(Integer.parseInt(entry.image));
+        holder.tvNameP.setText(entry.name);
+
+
+        return convertView;
+    }*/
 }
--- app/src/main/java/com/team2/saleftp/OrderActivity.java
+++ app/src/main/java/com/team2/saleftp/OrderActivity.java
@@ -1,13 +1,39 @@
 package com.team2.saleftp;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
 
+    private ImageView imvInfo;
+    private TextInputEditText tiedtName, tiedtPhone, tiedtAddress;
+    private ListView lvOrder;
+    private TextView tvAmount, tvTotal;
+    private Button btnOrder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        setTitle("TIẾN HÀNH ĐẶT HÀNG");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
+
+    private void analyze() {
+        
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
+
 }
