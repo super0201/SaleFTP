8cd9de599caf3a0e0436ca15cfff510397fcb905
tonvii1207 <vidtps06490@fpt.edu.vn>
2018-12-14 13:40:00 +0700

    1

---
+++ app/src/main/java/model/Cart.java
@@ -0,0 +1,60 @@
+package model;
+
+public class Cart {
+    public String idproduct;
+    public String name;
+    public String image;
+    public int amount;
+    public double price;
+
+    public Cart() {
+    }
+
+    public Cart(String idproduct, String name, String image, int amount, double price) {
+        this.idproduct = idproduct;
+        this.name = name;
+        this.image = image;
+        this.amount = amount;
+        this.price = price;
+    }
+
+    public String getIdproduct() {  
+        return idproduct;
+    }
+
+    public void setIdproduct(String idproduct) {
+        this.idproduct = idproduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
