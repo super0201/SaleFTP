0964c2cedbb68381e9d7bd4f1b36f08f81bb5db6
super0201 <super20411@gmail.com>
2018-12-16 00:37:51 +0700

    Update ProductDB.java

--- app/src/main/java/database/ProductDB.java
+++ app/src/main/java/database/ProductDB.java
@@ -18,6 +18,10 @@ public class ProductDB extends SQLiteOpenHelper {
         String sqlProd = "CREATE TABLE Product(ID text primary key," + "Name text, Price text, Detail text, Summary text, Image text)";
         db.execSQL(sqlProd);
 
+        String sqlDetail = "CREATE TABLE Detail(ID text primary key," + "Screen text, ScrRes, FrCam text, ReaCam text, CPU text, Ram text, Rom text, Sim text, MCard text, BattCapa text, OS text)";
+        db.execSQL(sqlDetail);
+
+        // insert basic info to Product
         sqlProd = "Insert Into Product values ( 'SM021','Samsung Galaxy Note 9','17.000.000 Đ', 'Điện thoại flagship đến từ Samsung đã nhanh chóng vượt mặt Apple trong cuộc đua phân khúc cao cấp', 'Flagship hàng đầu của Samsung ra mắt', 'https://i.imgur.com/gru5CzF.jpg')";
         db.execSQL(sqlProd);
 
@@ -50,6 +54,41 @@ public class ProductDB extends SQLiteOpenHelper {
 
         sqlProd = "Insert Into Product values ( 'HW20','Huawei Mate 20 Pro','24.000.000 Đ', 'Điện thoại flagship đến từ Huawei đã nhanh chóng vượt mặt Apple trong cuộc đua phân khúc cao cấp', 'Flagship hàng đầu của Huawei ra mắt', 'https://i.imgur.com/0vR2t75.jpg')";
         db.execSQL(sqlProd);
+
+        //insert detail Product
+
+        sqlDetail = "Insert Into Detail values ( 'SM01', 'Super AMOLED, 6.4\", Quad HD+ (2K+)', '1440 x 2960 pixels', '8 MP', 'Dual Camera 12 MP', 'Exynos 9810 8 nhân 64 bit', '6 GB', '128 GB', '1 Sim (Nano Sim)', 'MicroSD, hỗ trợ tối đa 512 GB', '4000 mAh', 'Android 8.0 (Oreo)')";
+        db.execSQL(sqlDetail);
+
+        sqlDetail = "Insert Into Detail values ( 'SO32', 'Super AMOLED, 6.4\", Quad HD+ (2K+)', '1440 x 2960 pixels', '8 MP', 'Dual Camera 12 MP', 'Exynos 9810 8 nhân 64 bit', '6 GB', '128 GB', '1 Sim (Nano Sim)', 'MicroSD, hỗ trợ tối đa 512 GB', '4000 mAh', 'Android 8.0 (Oreo)')";
+        db.execSQL(sqlDetail);
+
+        sqlDetail = "Insert Into Detail values ( 'XO24', 'Super AMOLED, 6.4\", Quad HD+ (2K+)', '1440 x 2960 pixels', '8 MP', 'Dual Camera 12 MP', 'Exynos 9810 8 nhân 64 bit', '6 GB', '128 GB', '1 Sim (Nano Sim)', 'MicroSD, hỗ trợ tối đa 512 GB', '4000 mAh', 'Android 8.0 (Oreo)')";
+        db.execSQL(sqlDetail);
+
+        sqlDetail = "Insert Into Detail values ( 'LG22', 'Super AMOLED, 6.4\", Quad HD+ (2K+)', '1440 x 2960 pixels', '8 MP', 'Dual Camera 12 MP', 'Exynos 9810 8 nhân 64 bit', '6 GB', '128 GB', '1 Sim (Nano Sim)', 'MicroSD, hỗ trợ tối đa 512 GB', '4000 mAh', 'Android 8.0 (Oreo)')";
+        db.execSQL(sqlDetail);
+
+        sqlDetail = "Insert Into Detail values ( 'XO43', 'Super AMOLED, 6.4\", Quad HD+ (2K+)', '1440 x 2960 pixels', '8 MP', 'Dual Camera 12 MP', 'Exynos 9810 8 nhân 64 bit', '6 GB', '128 GB', '1 Sim (Nano Sim)', 'MicroSD, hỗ trợ tối đa 512 GB', '4000 mAh', 'Android 8.0 (Oreo)')";
+        db.execSQL(sqlDetail);
+
+        sqlDetail = "Insert Into Detail values ( 'LG21', 'Super AMOLED, 6.4\", Quad HD+ (2K+)', '1440 x 2960 pixels', '8 MP', 'Dual Camera 12 MP', 'Exynos 9810 8 nhân 64 bit', '6 GB', '128 GB', '1 Sim (Nano Sim)', 'MicroSD, hỗ trợ tối đa 512 GB', '4000 mAh', 'Android 8.0 (Oreo)')";
+        db.execSQL(sqlDetail);
+
+        sqlDetail = "Insert Into Detail values ( 'IP8P', 'Super AMOLED, 6.4\", Quad HD+ (2K+)', '1440 x 2960 pixels', '8 MP', 'Dual Camera 12 MP', 'Exynos 9810 8 nhân 64 bit', '6 GB', '128 GB', '1 Sim (Nano Sim)', 'MicroSD, hỗ trợ tối đa 512 GB', '4000 mAh', 'Android 8.0 (Oreo)')";
+        db.execSQL(sqlDetail);
+
+        sqlDetail = "Insert Into Detail values ( 'SV1P', 'Super AMOLED, 6.4\", Quad HD+ (2K+)', '1440 x 2960 pixels', '8 MP', 'Dual Camera 12 MP', 'Exynos 9810 8 nhân 64 bit', '6 GB', '128 GB', '1 Sim (Nano Sim)', 'MicroSD, hỗ trợ tối đa 512 GB', '4000 mAh', 'Android 8.0 (Oreo)')";
+        db.execSQL(sqlDetail);
+
+        sqlDetail = "Insert Into Detail values ( 'HW8', 'Super AMOLED, 6.4\", Quad HD+ (2K+)', '1440 x 2960 pixels', '8 MP', 'Dual Camera 12 MP', 'Exynos 9810 8 nhân 64 bit', '6 GB', '128 GB', '1 Sim (Nano Sim)', 'MicroSD, hỗ trợ tối đa 512 GB', '4000 mAh', 'Android 8.0 (Oreo)')";
+        db.execSQL(sqlDetail);
+
+        sqlDetail = "Insert Into Detail values ( 'IPXS', 'Super AMOLED, 6.4\", Quad HD+ (2K+)', '1440 x 2960 pixels', '8 MP', 'Dual Camera 12 MP', 'Exynos 9810 8 nhân 64 bit', '6 GB', '128 GB', '1 Sim (Nano Sim)', 'MicroSD, hỗ trợ tối đa 512 GB', '4000 mAh', 'Android 8.0 (Oreo)')";
+        db.execSQL(sqlDetail);
+
+        sqlDetail = "Insert Into Detail values ( 'HW20', 'Super AMOLED, 6.4\", Quad HD+ (2K+)', '1440 x 2960 pixels', '8 MP', 'Dual Camera 12 MP', 'Exynos 9810 8 nhân 64 bit', '6 GB', '128 GB', '1 Sim (Nano Sim)', 'MicroSD, hỗ trợ tối đa 512 GB', '4000 mAh', 'Android 8.0 (Oreo)')";
+        db.execSQL(sqlDetail);
     }
 
     @Override
